2deb56766faebca318d0537cdd560c0d527c6417
Draug0 <85965310+Draug0@users.noreply.github.com>
2022-07-15 18:37:36 -0300

    RFC y credenciales

--- src/pages/invite/[invitation].tsx
+++ src/pages/invite/[invitation].tsx
@@ -1,12 +1,29 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
+import { CenterWrapper } from '../login'
+import RFC from '../../components/RFC'
+import CreatePassword from '../../components/CreatePassword'
 
 const Invitation = () => {
+  const [password, setPassword] = useState(false)
+
   const router = useRouter()
   const { invitation } = router.query
   console.log(router.query)
 
-  return <div>Usuario {invitation}</div>
+  if (password) {
+    return (
+      <CenterWrapper>
+        <CreatePassword />
+      </CenterWrapper>
+    )
+  }
+
+  return (
+    <CenterWrapper>
+      <RFC createPassword={setPassword} />
+    </CenterWrapper>
+  )
 }
 
 export default Invitation
